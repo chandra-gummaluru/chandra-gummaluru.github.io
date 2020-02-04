d490565521dfd0d8d97bfc5f7bf920b793a960b0
chandra-gummaluru <chandra.gummaluru@gmail.com>
2020-02-04 00:32:51 -0500

    Updates

---
+++ precache-manifest.77eb28fec31eecd08433fec7a3485ad4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a9fb481d0c59264cfa7e300a6c04fa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75d1e86525b3ce57983a",
+    "url": "/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "cc7c7163195b44067391",
+    "url": "/static/css/main.0857faae.chunk.css"
+  },
+  {
+    "revision": "75d1e86525b3ce57983a",
+    "url": "/static/js/2.7b9cb2ea.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/static/js/2.7b9cb2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7c7163195b44067391",
+    "url": "/static/js/main.48c136c0.chunk.js"
+  },
+  {
+    "revision": "0d46f7656c748826843c",
+    "url": "/static/js/runtime-main.c2b20a0b.js"
+  }
+]);
